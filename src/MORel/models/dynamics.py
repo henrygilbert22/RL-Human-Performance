33bc1c059333df2ad3ea743d53a2b5ee5afad721
henrygilbert22 <77060624+henrygilbert22@users.noreply.github.com>
2022-01-25 12:56:51 -0500

    updated dynamics

--- src/MORel/models/dynamics.py
+++ src/MORel/models/dynamics.py
@@ -2,9 +2,26 @@ from enviroment import Enviroment
 
 class Dynamics:
     
-    def __init__(self) -> None:
-        pass
-
+    look_behind: int
+    env: Enviroment
+    data: list
+    
+    X: list
+    Y: list
+    
+    def __init__(self, look_behind: int) -> None:
+        
+        self.look_behind = look_behind
+        self.env = Enviroment()
+        
+        self.data = self.env.get_dataset()
+        
+    def shift_data(self):
+        
+        for i in range(self.look_behind, len(self.data) - 1, 1):
+            self.X.append(self.data[i-self.look_behind: self.look_behind])
+            self.Y.append(self.data[i+1])
+        
 def main():
     d = Dynamics()
 
