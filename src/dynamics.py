0b7b3d2596e4480c380968897c1dd5a5c7e3f08d
Henry Gilbert <hg19b@my.fsu.edu>
2022-02-08 12:41:24 -0600

    env file

--- src/dynamics.py
+++ src/dynamics.py
@@ -21,6 +21,17 @@ class Dynamics:
     
     def __init__(self, look_behind: int, shift_amount: int) -> None:
         
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        
+        if gpus:
+            try:
+                for gpu in gpus:
+                    tf.config.experimental.set_memory_growth(gpu, True)
+            except RuntimeError as e:
+                print(e)
+
+        self.strategy = tf.distribute.MirroredStrategy(["GPU:0", "GPU:1"])
+
         self.look_behind = look_behind
         self.shift_amount = shift_amount
         self.env = Enviroment()
@@ -35,37 +46,59 @@ class Dynamics:
         
         for i in range(self.look_behind, len(self.data) - self.shift_amount, 1):
             
-            self.X.append(np.reshape(self.data[i-self.look_behind: i], (1, 5, 17)))    
-            self.Y.append(np.reshape(self.data[i+self.shift_amount-1][:-1], (1, 1, 16)))      # To remove the action in the output, we don't need to predict the next action, just next state
+            self.X.append(np.reshape(self.data[i-self.look_behind: i], (5, 17)))    
+            self.Y.append(np.reshape(self.data[i+self.shift_amount-1][:-1], (1, 16)))      # To remove the action in the output, we don't need to predict the next action, just next state
 
      
     def build_model(self):
         
-        model = keras.Sequential()
-        
-        model.add(LSTM(50, input_shape=(self.look_behind, 17 ), activation='relu', return_sequences=True, name="digits"))
-        model.add(keras.layers.Dropout(0.25))
+        with self.strategy.scope():
 
-        model.add(LSTM(50, activation='relu', return_sequences=True, name="sequence1"))
-        model.add(keras.layers.Dropout(0.2))
+            model = keras.Sequential()
+            
+            model.add(LSTM(3000, input_shape=(self.look_behind, 17 ), activation='relu', return_sequences=True, name="digits"))
+            model.add(keras.layers.Dropout(0.25))
 
-        model.add(LSTM(50, activation='relu', return_sequences=True, name="sequence2"))
-        model.add(keras.layers.Dropout(0.2))
+            model.add(LSTM(3000, activation='relu', return_sequences=True, name="sequence1"))
+            model.add(keras.layers.Dropout(0.2))
 
-        model.add(keras.layers.Dense((len(np.array(self.Y[0]).flatten())), name='finale'))
+            model.add(LSTM(3000, activation='relu', return_sequences=True, name="sequence2"))
+            model.add(keras.layers.Dropout(0.2))
 
-        model.compile(loss='mean_squared_error', optimizer='adam')
-        
-        self.model = model
+            model.add(LSTM(3000, activation='relu', return_sequences=True, name="sequence3"))
+            model.add(keras.layers.Dropout(0.2))
+
+            model.add(LSTM(3000, activation='relu', return_sequences=True, name="sequence4"))
+            model.add(keras.layers.Dropout(0.2))
+
+            model.add(keras.layers.Dense((len(np.array(self.Y[0]).flatten())), name='finale'))
+
+            model.compile(loss='mean_squared_error', optimizer='adam')
+            
+            self.model = model
     
     def train_model(self):
-                
+
         earlystopping = callbacks.EarlyStopping(monitor ="loss", mode ="min", patience = 10, restore_best_weights = True)
-        train_data = tf.data.Dataset.from_tensor_slices((self.X, self.Y))
+        train_data = tf.data.Dataset.from_tensor_slices((self.X[0:int(len(self.X)*0.8)], self.Y[0:int(len(self.X)*0.8)]))
+        train_data = train_data.batch(128)
+        options = tf.data.Options()
+        options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.OFF
+        train_data = train_data.with_options(options)
             
         self.model.fit(train_data, batch_size=256, epochs=100, callbacks=[earlystopping])
         
+        test_data = tf.data.Dataset.from_tensor_slices((self.X[int(len(self.X)*0.8):], self.Y[int(len(self.X)*0.8):]))
+        test_data = test_data.batch(128)
+        options = tf.data.Options()
+        options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.OFF
+        test_data = test_data.with_options(options)
+            
+
+        
+        
 def main():
+    
     d = Dynamics(5, 1)
 
 if __name__ == '__main__':
